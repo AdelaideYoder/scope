f597ab458cb9b53ecfd1b8b0d73a13031004a437
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-08 12:09:01 -0500

    worked on exercise 4

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-// This is exercise 1
-let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-let y = 0
+// // This is exercise 1
+// let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 0
 
-for ( y = 0; y < cookies.length; y++) {
-    const currentCookie = cookies[y]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
-}
+// for ( y = 0; y < cookies.length; y++) {
+//     const currentCookie = cookies[y]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
 
-// This is exercise 2
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-    console.log(conjoinedWord)
-}
+// // This is exercise 2
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
 
-conjunction("Master", "Card")
+// conjunction("Master", "Card")
 
 // This is excercie 4
 const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
 
-for (let k = 0; k < locations.length; k++) {
+let k; 
+
+for (k = 0; k < locations.length; k++) {
     let currentLocation = locations[k];
     let invalidLocation = false;
 
@@ -29,9 +31,9 @@ for (let k = 0; k < locations.length; k++) {
     if (invalidLocation) {
         console.log("This location is invalid")
     }
-    console.log(`There were ${k} locations displayed`)
 }
 
+console.log(`There were ${k} locations displayed`)
 
 
 
